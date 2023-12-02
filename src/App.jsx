b57445dbfbe56da36db77263edbd02ae5ccf02d2
eslamwageh <53353517+eslamwageh@users.noreply.github.com>
2023-12-03 01:33:35 +0200

    folders and vite app created

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>{/* <Route path="/" element={} /> */}</Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
