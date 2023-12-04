6a0d78915dd82632b15169ee5bebd9c5375f5477
eslamwageh <53353517+eslamwageh@users.noreply.github.com>
2023-12-04 19:42:38 +0200

    star rating comp added

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,10 @@
 import Checkout from "../../components/Checkout/Checkout";
+import StarRating from "../../components/StarRating/StarRating";
 function Homepage() {
   return (
     <div>
       <Checkout />
+      <StarRating />
     </div>
   );
 }
