1e90ba61c22586be27426eeca6601eda9f22d7ae
Adham-hussin <67987638+Adham-hussin@users.noreply.github.com>
2023-12-06 00:38:55 +0200

    ✨ Final Notifications Design

--- src/components/NotificationsContainer/NotificationsContainer.jsx
+++ src/components/NotificationsContainer/NotificationsContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect } from "react";
 import styles from "./NotificationsContainer.module.css";
+import Notification from '../Notification/Notification';
 
 function NotificationsContainer({opened}) {
     useEffect(() => {
@@ -12,7 +13,11 @@ function NotificationsContainer({opened}) {
     }, [opened]);
 
   return (
-    <div className= {styles.container}>NotificationsContainer</div>
+    <div className= {styles.container}>
+        <Notification title="New Order" description="You have a new order from a customer" />
+        <Notification title="New Barter" description="You have a barter request from a Seller" />
+        <Notification title="Auction Results" description="Your auction has ended, check the results" />
+    </div>
   )
 }
 
