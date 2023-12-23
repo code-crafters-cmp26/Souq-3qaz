9411e8746d668c6c66c38347ec4db5b9dfc57ca0
eslamwageh <53353517+eslamwageh@users.noreply.github.com>
2023-12-23 17:39:10 +0200

    product page changes

--- src/pages/Product/ProductPage.jsx
+++ src/pages/Product/ProductPage.jsx
@@ -29,25 +29,32 @@ function Productpage() {
     return <div>Loading...</div>;
   }
 
+  // Convert the string to a Date object
+  const dateObject = new Date(productData.putdate);
+
+  // Get the date in the format "YYYY-MM-DD"
+  const extractedDate = dateObject.toISOString().substring(0, 10);
+
   return (
     <div className={styles.productcontainer}>
       <div className={styles.productpage}>
         <img className={styles.img} src={productData.image} />
-        <div className={styles.content}>
+        <section className={styles.content}>
           <h1 className={styles.category}>{productData.category}</h1>
           <h1 className={styles.name}>{productData.name}</h1>
           <h2>Available quantity: {productData.quantity}</h2>
           <div>{productData.description}</div>
-          <div>Seller or Brand: {productData.seller}</div>
+          <div>Seller or Brand: {productData.name}</div>
           <div className={styles.reviews}></div>
           <hr />
           <p>{productData.price}$</p>
           <p className={styles.seller}></p>
-        </div>
-        <div className={styles.purchase}>
+        </section>
+        <section className={styles.purchase}>
+          <div className={styles.date}>Date of releasing: {extractedDate}</div>
           <Button text="Add to cart" />
           <Button text="Buy Now" />
-        </div>
+        </section>
       </div>
       <StarRating />
     </div>
