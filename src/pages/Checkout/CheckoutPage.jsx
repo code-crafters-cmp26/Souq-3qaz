48083959602d3cba433ace273518c3c982928d1c
Ashraf-Bahy <ashrafbahy286@gmail.com>
2023-12-24 02:08:55 +0200

    buying and wishing

--- src/pages/Checkout/CheckoutPage.jsx
+++ src/pages/Checkout/CheckoutPage.jsx
@@ -1,9 +1,23 @@
+import { useLocation } from "react-router-dom";
 import styles from "./CheckoutPage.module.css";
 import Checkout from "../../components/Checkout/Checkout";
 function CheckoutPage() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  // Access individual parameters
+  const productName = queryParams.get("name");
+  const productPrice = queryParams.get("price");
+  const productID = queryParams.get("id");
+  const productShipping = 1;
   return (
     <>
-      <Checkout />
+      <Checkout
+        productName={productName}
+        productPrice={productPrice}
+        productID={productID}
+        productShipping={productShipping}
+      />
       <div className={styles.checkoutpage}>
         <h3>1 Shipping address</h3>
         <hr />
