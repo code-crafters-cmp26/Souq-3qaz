a8d49e29875b1636d1a089d314802495c6e812ed
Adham-hussin <67987638+Adham-hussin@users.noreply.github.com>
2023-12-29 11:26:07 +0200

    ⚙️ Modify Seller Stat

--- src/pages/SellerStats/SellerStats.jsx
+++ src/pages/SellerStats/SellerStats.jsx
@@ -10,12 +10,14 @@ function SellerStats() {
       { Header: 'Transaction ID', accessor: 'transactionid' },
       { Header: 'Customer ID', accessor: 'customerid' },
       { Header: 'Product ID', accessor: 'productid' },
+      { Header: 'Product Name', accessor: 'name' },
       { Header: 'Date', accessor: 'date' },
       { Header: 'From Auction', accessor: 'fromauction' },
       { Header: 'Returned', accessor: 'returned' },
       { Header: 'Shipped Via', accessor: 'shippedvia' },
       { Header: 'Quantity', accessor: 'quantity' },
       { Header: 'Shipment ID', accessor: 'shipmentid' },
+
     ],
     []
   );
@@ -33,6 +35,19 @@ function SellerStats() {
       })
       .then((data) => {
         setSellerData(data.retult);
+        // itirate over data.retult[i].fromauction and set it to yes or no and same for returned
+        for (let i = 0; i < data.retult.length; i++) {
+            if (data.retult[i].fromauction == 1) {
+                data.retult[i].fromauction = 'Yes';
+            } else {
+                data.retult[i].fromauction = 'No';
+            }
+            if (data.retult[i].returned == 1) {
+                data.retult[i].returned = 'Yes';
+            } else {
+                data.retult[i].returned = 'No';
+            }
+        }
       })
       .catch((error) => {
         console.log(error.message);
