65111e7f045ff4589ebbea88a9d1d8d545078328
Ashraf-Bahy <ashrafbahy286@gmail.com>
2023-12-29 10:46:29 +0200

    handling merging error

--- BackEnd/Server/controllers/userController.js
+++ BackEnd/Server/controllers/userController.js
@@ -158,20 +158,6 @@ exports.getWish = catchAsync(async (req, res) => {
   });
 });
 
-exports.getWish = catchAsync(async (req, res) => {
-  const customerId = req.user["rows"][0]["id"];
-
-  console.log(customerId);
-  const result = await db.query(
-    `SELECT * FROM wishlist WHERE customerid=${customerId};`
-  );
-
-  res.status(200).json({
-    status: "success",
-    result: result["rows"],
-  });
-});
-
 exports.updateInfo = catchAsync(async (req, res, next) => {
   const userId = req.user["rows"][0]["id"];
   const {
